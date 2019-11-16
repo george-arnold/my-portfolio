5782a7c40d5938de57a70a25da48fab71e2ab0c8
George Arnold <george.arnold.88@gmail.com>
2019-11-16 13:56:37 -0500

    final draft of portfolio

---
+++ main.js
@@ -0,0 +1,45 @@
+// add an event listener that shows and hides HTML for each section  (this will involve absolute positioning of content)
+
+
+function handleNavigation() {
+
+  $(".tab-1").click(function() {
+    $(this).addClass("active-tab");
+    $(".tab-2").removeClass("active-tab");
+    $(".tab-3").removeClass("active-tab");
+    $(".project").show();
+    $(".bio").hide();
+    $(".contact-info").hide();
+  });
+
+  $(".tab-2").click(function() {
+    $(this).addClass("active-tab");
+    $(".tab-1").removeClass("active-tab");
+    $(".tab-3").removeClass("active-tab");
+    $(".project").hide();
+    $(".bio").show();
+    $(".contact-info").hide();
+  });
+
+  $(".tab-3").click(function() {
+    $(this).addClass("active-tab");
+    $(".tab-1").removeClass("active-tab");
+    $(".tab-2").removeClass("active-tab");
+    $(".project").hide();
+    $(".bio").hide();
+    $(".contact-info").show();
+  });
+
+}
+
+
+
+
+
+
+function start () {
+    $(".project").hide();
+    $(".contact-info").hide();
+    handleNavigation();
+}
+$(start);
